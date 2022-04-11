f75c2578437dc43e58f384075981b6d6a1d6a109
Craig Opie <craig@deathstardev.com>
2022-04-11 13:39:43 -1000

    init

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # natural_language_processing_in_prolog
 Natural Language Processing in Prolog
+
+##Instructions
+###Natural Language Processing in Prolog
+
+Rules: Cite any sources you use, and write your own comments and error messages. Do not use any built-in predicates (aside from arithmetic operators, write/1 and format/2).
+
+See Ass5.pl (under "Resources"). This will give you a head start on this assignment. You’ll have to modify it a lot, though!
+
+Your task is to write a DCG and a database (i.e. a set of predicates) to answer questions about the Star Wars movies. Use IMDB.com as your source. Your database should cover any three movies in the series, and include information about five actors/characters for each movie, as well as its director. For consistency (and for the geeks amongst us, including myself), "The Phantom Menace" is Episode 1, and "A New Hope" is Episode 4. 
+
+The top level predicate (top/1) should take a wide range of questions (represented in list form) as its argument, and write out a good answer to the question. It should write out “I don’t get it” if the question is ungrammatical. If the question is grammatical but nonsensical (e.g. “Did Star Wars IV play Luke?”) it should simply give a negative response. If there are multiple answers to a question, forced backtracking (i.e. hitting “;”) should produce them.
+
+You should handle agreement with features.
+
+Grammatical queries:
+
+“Did Mark Hamill play Luke Skywalker?”
+
+“Mark Hamill plays Luke Skywalker, right?”
+
+“Did Mark Hamill play Han Solo in Star Wars III?”
+
+“Did Mark Hamill direct Star Wars I?”
+
+“Is it true that Mark Hamill is an actor?”
+
+"Is it true that Han Solo is a character in Star Wars III?"
+
+“Is it true that Princess Leia is an actor and Mark Hamill is a director?”
+
+“George Lucas is a director, Mark Hamill is an actor and Han Solo is a character, right?” [should be able to handle any number of subqueries]
+
+“Who is the actor for Han Solo?”
+
+“What is the title of Star Wars II?”
+
+“Who is the director of Star Wars II?”
+
+“Who is the character of Mark Hamill?”
+
+Ungrammatical queries (examples):
+
+* “Did Mark Hamill plays Luke Skywalker?”
+
+* “Is it true that Mark Hamill is a actor?”
+
+* “Mark Hamill play Chewbacca, right?”
+
+* “George Lucas is an director, right?”
+
+###Bonus (10 points)
+
+Impress me – be creative! Allow a wider range of queries than specified above, without allowing ungrammatical queries. To get full points, provide a list of grammatical queries allowed by your expanded DCG, as well as a list of ungrammatical queries it should reject.
+
+###What to submit
+
+Turn in the following (all via Laulima):
+
+All code, appropriately commented.
+
+All transcripts, showing that the code performs as required on a representative set of inputs. 
+
+Additional resources for assignment
+No attachments yet
---
+++ opieca5.pl
@@ -0,0 +1,150 @@
+#!/usr/bin/env swipl
+/*  Author:     Craig Opie
+    Email:      opieca@hawaii.edu
+    Assignment: No. 5
+    References: https://www.swi-prolog.org/pldoc/doc_for?object=dif/2
+
+    Test Cases: top([is, it, true, that, mark, hamill, acts, in, star, wars, iv]).
+                top([who, acts, in, star, wars, iv]).
+*/
+
+/*
+    should write out a sensible answer regardless if it passes or fails
+    it really should write out the text of the answer, not just the symbol.
+*/
+top(Sentence) :- yesno(Query, Sentence, []), showresults(Query).
+top(Sentence) :- who(Who, Sentence, []), write("The person you're looking for is "), write(Who).
+
+
+
+/*
+    writes out positive text if ARG1 is a list of true predicates, negative otherwise.
+*/
+showresults(Query) :- test(Query), write("Yes, that's true.").
+showresults(_) :- test(Query), write("Sorry, that's false.").
+
+
+
+/*
+    takes a list of predicates and succeeds if all the predicates are true, otherwise fails.
+*/
+test([Query]) :- Query.
+test([Query|Rest]) :- Query, test(Rest).
+
+
+/*
+    grammer section
+*/
+who(X) --> [who], verb_phrase(X^_^[Query]), {Query}.
+yesno(Sem) --> [is, it, true, that], statement(_^_^Sem). 
+statement(S) --> singlestatement(S).
+statement(_^_^Sem) --> singlestatement(_^_^S1), [and], statement(_^_^S2), {append(S1,S2,Sem)}.
+singlestatement(Subj^Obj^Sem) --> noun_phrase(Subj), verb_phrase(Subj^Obj^Sem).
+noun_phrase(Sem) --> proper_noun(Sem).
+verb_phrase(Subj^Obj^Sem) --> verb(Subj^Obj^Sem), noun_phrase(Obj).
+proper_noun(mark_hamill) --> [mark, hamill].
+proper_noun(harrison_ford) --> [harrison, ford].
+proper_noun(star_wars4) --> [star, wars, iv].
+proper_noun(star_wars3) --> [star, wars, iii].
+verb(X^Y^[acts_in(X,Y)]) --> [acts, in].
+
+
+/*
+    database
+*/
+actor(liam_neeson).
+actor(ewan_mcgregor).
+actor(natalie_portman).
+actor(jake_lloyd).
+actor(ian_mcdiarmid).
+actor(pernilla_august).
+
+actor(mark_hamill).
+actor(harrison_ford).
+actor(carrie_fisher).
+actor(alec_guinness).
+actor(peter_cushing).
+actor(anthony_daniels).
+actor(kenny_baker).
+actor(peter_mayhew).
+actor(david_prowse).
+actor(phil_brown).
+actor(shelagh_fraser).
+actor(jack_purvis).
+
+director(george_lucas).
+director(j_j_abrams).
+director(rian_johnson).
+
+writer(george_lucas).
+
+acts_in(liam_neeson, star_wars1).
+acts_in(ewan_mcgregor, star_wars1).
+acts_in(natalie_portman, star_wars1).
+acts_in(jake_lloyd, star_wars1).
+acts_in(ian_mcdiarmid, star_wars1).
+acts_in(pernilla_august, star_wars1).
+acts_in(mark_hamill, star_wars4).
+acts_in(harrison_ford, star_wars4).
+acts_in(carrie_fisher, star_wars4).
+acts_in(alec_guinness, star_wars4).
+acts_in(peter_cushing, star_wars4).
+acts_in(anthony_daniels, star_wars1).
+acts_in(anthony_daniels, star_wars4).
+acts_in(kenny_baker, star_wars1).
+acts_in(kenny_baker, star_wars4).
+acts_in(peter_mayhew, star_wars4).
+acts_in(david_prowse, star_wars4).
+acts_in(phil_brown, star_wars4).
+acts_in(shelagh_fraser, star_wars4).
+acts_in(jack_purvis, star_wars4).
+
+plays(liam_neeson, qui-gon_jinn).
+plays(ewan_mcgregor, obi-wan_kenobi).
+plays(natalie_portman, queen_amidala).
+plays(natalie_portman, padme).
+plays(jake_lloyd, anakin_skywalker).
+plays(ian_mcdiarmid, senator_palpatine).
+plays(pernilla_august, shmi_skywalker).
+plays(mark_hamill, luke_skywalker).
+plays(harrison_ford, han_solo).
+plays(carrie_fisher, princess_leia).
+plays(alec_guinness, obi-wan_kenobi).
+plays(peter_cushing, grand_moff_tarkin).
+plays(anthony_daniels, c-3po).
+plays(kenny_baker, r2-d2).
+plays(peter_mayhew, chewbacca).
+plays(david_prowse, darth_vader).
+plays(phil_brown, uncle_owen).
+plays(shelagh_fraser, aunt_beru).
+plays(jack_purvis, chief_jawa).
+
+directs(star_wars1, george_lucas).
+directs(star_wars2, george_lucas).
+directs(star_wars3, george_lucas).
+directs(star_wars4, george_lucas).
+directs(star_wars5, george_lucas).
+directs(star_wars6, george_lucas).
+directs(star_wars7, j_j_abrams).
+directs(star_wars8, rian_johnson).
+directs(star_wars9, j_j_abrams).
+
+writes(star_wars1, george_lucas).
+writes(star_wars2, george_lucas).
+writes(star_wars3, george_lucas).
+writes(star_wars4, george_lucas).
+writes(star_wars5, george_lucas).
+writes(star_wars6, george_lucas).
+writes(star_wars7, j_j_abrams).
+writes(star_wars8, rian_johnson).
+writes(star_wars9, j_j_abrams).
+
+title(star_wars1, the_phantom_menace).
+title(star_wars2, attack_of_the_clones).
+title(star_wars3, revenge_of_the_sith).
+title(star_wars4, a_new_hope).
+title(star_wars5, the_empire_strikes_back).
+title(star_wars6, return_of_the_jedi).
+title(star_wars7, the_force_awakens).
+title(star_wars8, the_last_jedi).
+title(star_wars9, the_rise_of_skywalker).
