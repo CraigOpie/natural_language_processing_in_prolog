9bb4160af5a610196b9eb906c7c4f5f2cfad3631
Craig Opie <craig@deathstardev.com>
2022-04-11 14:26:45 -1000

    added content

--- opieca5.pl
+++ opieca5.pl
@@ -37,15 +37,48 @@ test([Query|Rest]) :- Query, test(Rest).
 */
 who(X) --> [who], verb_phrase(X^_^[Query]), {Query}.
 yesno(Sem) --> [is, it, true, that], statement(_^_^Sem). 
+
 statement(S) --> singlestatement(S).
 statement(_^_^Sem) --> singlestatement(_^_^S1), [and], statement(_^_^S2), {append(S1,S2,Sem)}.
+
 singlestatement(Subj^Obj^Sem) --> noun_phrase(Subj), verb_phrase(Subj^Obj^Sem).
+
 noun_phrase(Sem) --> proper_noun(Sem).
 verb_phrase(Subj^Obj^Sem) --> verb(Subj^Obj^Sem), noun_phrase(Obj).
+
+proper_noun(liam_neeson) --> [liam, neeson].
+proper_noun(ewan_mcgregor) --> [ewan, mcgregor].
+proper_noun(natalie_portman) --> [natalie, portman].
+proper_noun(jake_lloyd) --> [jake, lloyd].
+proper_noun(ian_mcdiarmid) --> [ian, mcdiarmid].
+proper_noun(pernilla_august) --> [pernilla, august].
 proper_noun(mark_hamill) --> [mark, hamill].
 proper_noun(harrison_ford) --> [harrison, ford].
-proper_noun(star_wars4) --> [star, wars, iv].
+proper_noun(carrie_fisher) --> [carrie, fisher].
+proper_noun(alec_guinness) --> [alec, guinness].
+proper_noun(peter_cushing) --> [peter, cushing].
+proper_noun(anthony_daniels) --> [anthony, daniels].
+proper_noun(kenny_baker) --> [kenny, baker].
+proper_noun(peter_mayhew) --> [peter, mayhew].
+proper_noun(david_prowse) --> [david, prowse].
+proper_noun(phil_brown) --> [phil, brown].
+proper_noun(shelagh_fraser) --> [shelagh, fraser].
+proper_noun(jack_purvis) --> [jack, purvis].
+
+proper_noun(george_lucas) --> [george, lucas].
+proper_noun(j_j_abrams) --> [j, j, abrams].
+proper_noun(rian_johnson) --> [rian, johnson].
+
+proper_noun(star_wars1) --> [star, wars, i].
+proper_noun(star_wars2) --> [star, wars, ii].
 proper_noun(star_wars3) --> [star, wars, iii].
+proper_noun(star_wars4) --> [star, wars, iv].
+proper_noun(star_wars5) --> [star, wars, v].
+proper_noun(star_wars6) --> [star, wars, vi].
+proper_noun(star_wars7) --> [star, wars, vii].
+proper_noun(star_wars8) --> [star, wars, viii].
+proper_noun(star_wars9) --> [star, wars, ix].
+
 verb(X^Y^[acts_in(X,Y)]) --> [acts, in].
 
 
@@ -80,14 +113,26 @@ writer(george_lucas).
 
 acts_in(liam_neeson, star_wars1).
 acts_in(ewan_mcgregor, star_wars1).
+acts_in(ewan_mcgregor, star_wars2).
+acts_in(ewan_mcgregor, star_wars3).
 acts_in(natalie_portman, star_wars1).
+acts_in(natalie_portman, star_wars2).
+acts_in(natalie_portman, star_wars3).
 acts_in(jake_lloyd, star_wars1).
 acts_in(ian_mcdiarmid, star_wars1).
 acts_in(pernilla_august, star_wars1).
 acts_in(mark_hamill, star_wars4).
+acts_in(mark_hamill, star_wars5).
+acts_in(mark_hamill, star_wars6).
 acts_in(harrison_ford, star_wars4).
+acts_in(harrison_ford, star_wars5).
+acts_in(harrison_ford, star_wars6).
+acts_in(harrison_ford, star_wars7).
 acts_in(carrie_fisher, star_wars4).
-acts_in(alec_guinness, star_wars4).
+acts_in(carrie_fisher, star_wars5).
+acts_in(carrie_fisher, star_wars6).
+acts_in(carrie_fisher, star_wars7).
+acts_in(alec_guinness, star_wars8).
 acts_in(peter_cushing, star_wars4).
 acts_in(anthony_daniels, star_wars1).
 acts_in(anthony_daniels, star_wars4).
@@ -111,8 +156,8 @@ plays(harrison_ford, han_solo).
 plays(carrie_fisher, princess_leia).
 plays(alec_guinness, obi-wan_kenobi).
 plays(peter_cushing, grand_moff_tarkin).
-plays(anthony_daniels, c-3po).
-plays(kenny_baker, r2-d2).
+plays(anthony_daniels, c3po).
+plays(kenny_baker, r2d2).
 plays(peter_mayhew, chewbacca).
 plays(david_prowse, darth_vader).
 plays(phil_brown, uncle_owen).
